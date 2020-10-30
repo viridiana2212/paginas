86517b43346b3cca848efea092c046d6d0c6c341
viridiana2212 <viridianalopez062@gmail.com>
2020-10-30 12:05:45 -0600

    version 2

---
+++ Angular/aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -0,0 +1,8 @@
+import { Component} form '@angular/core';
+@Component({selector:'zapatillas',
+templateUrl:'./zapatillas.component.html'
+})
+export class ZapatillasComponent{
+	public titulo:string="componnete de zapatillas";
+	
+}
